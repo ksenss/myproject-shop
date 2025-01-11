d2ebd066840a7c3bbcd64fd27782d1baed0b894b
John Doe <johndoe@example.com>
2025-01-11 13:16:20 +0200

    hyryjht

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
+
+
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name= models.CharField(max_length=255)
+    price = models.FloatField()
+    category = models.CharField(max_length=255)
+    createdAt = models.DateField(default=datetime.now)
+    inStock = models.BooleanField(default=False)
+    description = models.TextField(null=True)
+    image = models.CharField(max_length=2083)
+
+    def __str__(self):
+        return self.title
--- products/views.py
+++ products/views.py
@@ -1,10 +1,27 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from datetime import datetime
+
+from products.models import Product
+
 
 # Create your views here.
 def index(request):
-     return render(request, "index.html")
+     products = Product.objects.all()
+     return render(request, "index.html", { "products": products })
+def delete(request, id):
+    product = Product.objects.get(id=id)
 
+    if product is None:
+        return HttpResponse("Product not found", status=404)
+    
+    product.delete()
+    return redirect("/")
 
-def gallery(request):
-     return render(request, "gallery.html")
+def details(request):
+    
+    return HttpResponse(
+        """<h1>Product Details</h1> 
+        <hr> 
+        <p>Product details page</p>"""
+    )
